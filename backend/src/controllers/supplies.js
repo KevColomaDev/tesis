1ee117db9ab4ae3efcc92b1724fb8e49419ecb3f
Isaac4Lion <isaac1leon@gmail.com>
2024-10-29 14:27:01 -0500

    feat: Endpoints to assign supplies to rooms

--- backend/src/controllers/supplies.js
+++ backend/src/controllers/supplies.js
@@ -2,6 +2,8 @@ import { supplies, collectionSuplies } from '../models/supplies.js'
 import { validateSupply } from '../schemas/createSupply.js'
 import { validateAddSupplies } from '../schemas/addSupplies.js'
 import mongoose from 'mongoose'
+import { validateAssignSupplies } from '../schemas/assignSupplies.js'
+import { collectionReportsSupplies } from '../models/reportsSupplies.js'
 
 export const getSupplies = async (req, res) => {
   try {
@@ -83,23 +85,39 @@ export const addStock = async (req, res) => {
 }
 
 export const assignSupplies = async (req, res) => {
-  // try {
-  //   const suppliesInput = validateAddSupplies(req.body)
-  //   console.log(suppliesInput)
-  //   const oldSupplies = await collectionSuplies.findOne({ name: suppliesInput.name })
-  //   if (oldSupplies.toothPaste < suppliesInput.toothPaste || oldSupplies.soap < suppliesInput.soap || oldSupplies.toothBrush < suppliesInput.toothBrush || oldSupplies.towel < suppliesInput.towel) {
-  //     return res.status(401).json({ msg: 'Not enough supplies' })
-  //   }
-
-  //   const newSupplies = {
-  //     toothPaste: oldSupplies.toothPaste - suppliesInput.toothPaste,
-  //     soap: oldSupplies.soap - suppliesInput.soap,
-  //     toothBrush: oldSupplies.toothBrush - suppliesInput.toothBrush,
-  //     towel: oldSupplies.towel - suppliesInput.towel
-  //   }
-  //   await supplies.registerSupplies(newSupplies)
-  //   return res.status(200).json({ msg: 'Supplies assigned', newSupplies })
-  // } catch (error) {
-  //   console.log(error)
-  // }
+  try {
+    const { room: numberRoom } = req.params
+    const assignInput = validateAssignSupplies(req.body)
+    if (Object.keys(assignInput).includes('issues')) {
+      return res.status(400).json({ errors: assignInput.issues })
+    }
+    const updatedSupplies = []
+    // Check if existences exists
+    for (let i = 0; i < assignInput.supplies.length; i++) {
+      try {
+        const { quantity } = await collectionSuplies.findOne({ name: assignInput.supplies[i].name })
+        if (quantity < assignInput.supplies[i].quantity) {
+          return res.status(400).json({ msg: 'Not enough existencies.' })
+        } else {
+          const updatedSupply = await collectionSuplies.findOneAndUpdate(
+            { name: assignInput.supplies[i].name },
+            { $set: { quantity: quantity - assignInput.supplies[i].quantity } },
+            { returnDocument: 'after' })
+          updatedSupplies.push(updatedSupply)
+        }
+      } catch (error) {
+        return res.status(400).json({ msg: 'Supply or supplies doesnt exist.' })
+      }
+    }
+    // Make report
+    const report = {
+      room: numberRoom,
+      assignedSupplies: assignInput.supplies,
+      assignDate: new Date().toLocaleDateString()
+    }
+    await collectionReportsSupplies.insertOne(report)
+    return res.status(200).json(report)
+  } catch (error) {
+    console.log(error)
+  }
 }
