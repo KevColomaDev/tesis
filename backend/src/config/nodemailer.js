524df6e40b37a8fbdce1b95ec08c577319c4913f
Isaac4Lion <isaac1leon@gmail.com>
2024-11-22 11:39:03 -0500

    fix: nodemailer config

--- backend/src/config/nodemailer.js
+++ backend/src/config/nodemailer.js
@@ -1,53 +1,51 @@
-import nodemailer from "nodemailer";
+import nodemailer from 'nodemailer';
 import dotenv from 'dotenv';
 
-dotenv.config();
+dotenv.config()
 
-let transporter = nodemailer.createTransport({
-    service: 'gmail',
-    host: process.env.HOST_MAILTRAP,
-    port: process.env.PORT_MAILTRAP,
-    auth: {
-        user: process.env.USER_MAILTRAP,
-        pass: process.env.PASS_MAILTRAP,
-    }
-});
-
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  host: process.env.HOST_MAILTRAP,
+  port: process.env.PORT_MAILTRAP,
+  auth: {
+    user: process.env.USER_MAILTRAP,
+    pass: process.env.PASS_MAILTRAP
+  }
+})
 
 export const sendDonationNotification = (userMail, donationDetails) => {
-    let mailOptions = {
-        from: process.env.USER_MAILTRAP,
-        to: userMail,
-        subject: "¡Has recibido una nueva donación!",
-        html: `<p>Hola, se te ha asignado una nueva donación: <strong>${donationDetails}</strong></p>`,
-    };
+  const mailOptions = {
+    from: process.env.USER_MAILTRAP,
+    to: userMail,
+    subject: '¡Has recibido una nueva donación!',
+    html: `<p>Hola, se te ha asignado una nueva donación: <strong>${donationDetails}</strong></p>`
+  }
 
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Correo de donación enviado: ' + info.response);
-        }
-    });
-};
-
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Correo de donación enviado: ' + info.response)
+    }
+  })
+}
 
 export const sendSupplyShortageNotification = (userMail, supplyItem) => {
-    let mailOptions = {
-        from: process.env.USER_MAILTRAP,
-        to: userMail,
-        subject: "¡Alerta de escasez de suministros!",
-        html: `<p>Hola, se ha detectado que queda poca cantidad del suministro: <strong>${supplyItem}</strong>. Por favor, considera reabastecerlo.</p>`,
-    };
-    
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-        } else {
-            console.log('Correo de escasez de suministros enviado: ' + info.response);
-        }
-    });
-};
+  const mailOptions = {
+    from: process.env.USER_MAILTRAP,
+    to: userMail,
+    subject: '¡Alerta de escasez de suministros!',
+    html: `<p>Hola, se ha detectado que queda poca cantidad del suministro: <strong>${supplyItem}</strong>. Por favor, considera reabastecerlo.</p>`
+  }
+
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Correo de escasez de suministros enviado: ' + info.response)
+    }
+  })
+}
 
 export const sendMailToAdmin = (userMail, password) => {
   const mailOptions = {
@@ -74,18 +72,18 @@ export const sendMailToSocialWorker = (userMail, password, token) => {
     from: process.env.USER_MAILTRAP,
     to: userMail,
     subject: '¡Bienvenido al equipo!',
-    html: `<p>Has sido registrado en Solca para ingresar nuestra plataforma verifica tu correo mediante el siguiente link: ${process.env.URL_FRONTEND}verificar-token/${token}</p>
+    html: `<p>Has sido registrado en Solca para ingresar nuestra plataforma</p>
     <br>
     <p style="font-weight: bold;">Tus credenciales son las siguientes:</p>
     <p>Correo electrónico: ${userMail}</p>
     <p>Contraseña: ${password}</p>
-    <p>No olvides restablecer tu contraseña.`
+    <p>No olvides restablecer tu contraseña.`,
   }
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.log(error)
     } else {
-      console.log('Correo de registro exitoso enviado: ' + info.response)
+      console.log('Correo de registro exitoso enviado: ' + info.response);
     }
   })
 }
