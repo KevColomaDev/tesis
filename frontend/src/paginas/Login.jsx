a9cee8630dfd1856b9dcd82cfaf56a80da04ccfc
sevastrl <sebastianhcv5@gmail.com>
2024-12-10 20:16:57 -0500

    Logo

--- frontend/src/paginas/Login.jsx
+++ frontend/src/paginas/Login.jsx
@@ -43,7 +43,7 @@ export default function Login() {
           alt="Solca"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-sky-900">
-          Centro de Hospitalidad Solca
+          Centro de Hospitalidad
         </h2>
       </div>
 
